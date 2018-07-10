1ab006b5581a944dba53ea208943485178ade253
Ruben Maurer <rubenmaurer@live.de>
2018-07-10 19:09:00 +0200

    Add settings copy to build process

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
 dotnet restore
 dotnet build -c win10-x64
-dotnet publish -c Release -r win10-x64
+dotnet publish -c Release -r win10-x64 --self-contained
+
+copy settings.json bin\win10-x64\netcoreapp2.1
+copy settings.json bin\Release\netcoreapp2.1\win10-x64
